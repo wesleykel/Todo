d8f22322f1ce382b8b56ea4891322fc3620bd9a7
Wesley Kelly <wesley.kelly@hotmail.com>
2021-04-30 14:33:46 +0100

    can now add to do's  to page

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const newTodo = {
   arrayOfThings: [],
 
-  getToDo: function () {
+  displayTodo: function () {
     let input = document.querySelector("#thing-todo");
+
+    const newLiList = document.querySelector("#list-item ");
+    const newLi = document.createElement("li");
+    newLi.classList.add("new-todo");
+    newLiList.appendChild(newLi);
+    newLi.innerHTML = `${input.value}`;
+    newArray = this.arrayOfThings.push(input.value);
+    console.log(this.arrayOfThings);
+  },
+
+  /*getToDo: function () {
+    
     newArray = this.arrayOfThings.push(input.value);
     console.log(newArray);
-  },
+  },*/
 
-  displayTodo: function () {
-    console.log(this.arrayOfThings);
-  },
-
-  deleteTodo: function () {
-    this.arrayOfThings.pop();
-    console.log(this.arrayOfThings);
-  },
+  //deleteTodo: function () {
+  // this.arrayOfThings.pop();
+  // console.log(this.arrayOfThings);
+  // },
 };
