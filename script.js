627c471a6b308664e6d15ce0391846623ab662a1
Wesley Kelly <wesley.kelly@hotmail.com>
2021-05-01 15:38:31 +0100

    Project taken another  direction

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const newTodo = {
-  arrayOfThings: [],
+let arrayOfThings = [];
 
-  displayTodo: function () {
-    let input = document.querySelector("#thing-todo");
-
+function displayTodo() {
+  let input = document.querySelector("#thing-todo");
+  if (input.value === "") {
+    console.log("empty");
+  } else {
     const newLiList = document.querySelector("#list-item ");
     const newLi = document.createElement("li");
     newLi.classList.add("new-todo");
     newLiList.appendChild(newLi);
-    newLi.innerHTML = `${input.value}`;
-    newArray = this.arrayOfThings.push(input.value);
-    console.log(this.arrayOfThings);
-  },
+    newLi.innerHTML = `${input.value}
+      <span class="del-button"><i type ="button" class="fas fa-trash-alt"></i></span>
+   `;
 
-  /*getToDo: function () {
-    
-    newArray = this.arrayOfThings.push(input.value);
-    console.log(newArray);
-  },*/
+    newArray = arrayOfThings.push(input.value);
+    console.log(arrayOfThings);
 
-  //deleteTodo: function () {
-  // this.arrayOfThings.pop();
-  // console.log(this.arrayOfThings);
-  // },
-};
+    input.value = "";
+  }
+}
+function deleteTodo() {
+  console.log("run");
+  const delLi = document.querySelector(".new-todo");
+  delLi.parentNode.removeChild(delLi);
+}
+const delBut = document.querySelector(".fas fa-trash-alt");
+if (delBut === null) {
+  delBut.addEventListener("click", deleteTodo, false);
+}
+const but = document.querySelector(".b1");
+but.addEventListener("click", displayTodo, false);
